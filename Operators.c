2c075f57283a43faed5a24923011500bab9fe889
ramakrishnapakala <ramakrishnapakala25@gmail.com>
2026-09-22 15:38:43 +0530

    done

--- Operators.c
+++ Operators.c
@@ -11,7 +11,30 @@ int main(void){
     printf("division : %d\n",a/b);
     printf("reminder : %d\n",a%b);
     */
-   printf("%d\n",)
+//    relational operators
+   printf("%d\n",5 > 2);
+   printf("%d\n",5 < 2);
+   printf("%d\n",2 < 2);
+   printf("%d\n",2 <= 2);
+   printf("%d\n",2 != 2);
+   printf("%d\n",2 != 3);
+   printf("%d\n",2 == 2);
+//    logical operators
+int age = 19;
+printf("%d\n",age > 18 && age < 60);
+printf("%d\n",0 < 5 && 5 > 0);
+printf("%d\n",5 > 5 && 5 > 10);
+printf("%d\n",5 > 5 || 5 > 10);
+// increment and decrement 
+int a = 2;
+++a;
+printf("%d",a);
+a++;
+printf("%d",a);
+a += 5;
+printf("%d\n",a);
+a -= 5;
+printf("%d\n",a);
 
 
 }
